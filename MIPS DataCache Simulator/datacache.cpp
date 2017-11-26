d2cbd9d31c385fff2185550a8a67b8d6daed872a
Andrew Wood <AJWOOD@andrew-woods-imac.local>
2017-11-26 14:42:27 -0500

    Start of Project.

--- MIPS DataCache Simulator/datacache.cpp
+++ MIPS DataCache Simulator/datacache.cpp
@@ -3,7 +3,11 @@
  CDA3101 Project 6 - MIPS DataCache Simulator
  November 16, 2017
  
- Program description here.
+ The program performs the following in order:
+ 
+ 1) Reads the configuration file using the ReadConfig() function to determine the line size as well as the set configuration.
+ 2) Prints the configuration using the PrintConfig() function.
+ 
  
  Note that the code is self-documenting.
  */
